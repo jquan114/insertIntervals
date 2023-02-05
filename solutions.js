c7308df66000ed199f2bf50ee59d4d53efc68e2a
JQUAN COLLINS <104362301+jquan114@users.noreply.github.com>
2023-02-04 20:20:41 -0500

    Create solutions.js

---
+++ solutions.js
@@ -0,0 +1,23 @@
+You are given an array of non-overlapping intervals intervals where intervals[i] = [starti, endi] represent the start and the end of the ith interval and intervals is sorted in ascending order by starti. You are also given an interval newInterval = [start, end] that represents the start and end of another interval.
+
+Insert newInterval into intervals such that intervals is still sorted in ascending order by starti and intervals still does not have any overlapping intervals (merge overlapping intervals if necessary).
+
+Return intervals after the insertion.
+
+
+function insert(intervals, newInterval) {
+  let i = 0, res = [];
+  while (i < intervals.length && intervals[i][1] < newInterval[0]) {
+    res.push(intervals[i++]);
+  }
+  while (i < intervals.length && intervals[i][0] <= newInterval[1]) {
+    newInterval[0] = Math.min(newInterval[0], intervals[i][0]);
+    newInterval[1] = Math.max(newInterval[1], intervals[i][1]);
+    i++;
+  }
+  res.push(newInterval);
+  while (i < intervals.length) {
+    res.push(intervals[i++]);
+  }
+  return res;
+}
